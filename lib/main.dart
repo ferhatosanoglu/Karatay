890d819c66578a44db98b86fbdb110967cf3b47e
your_username <your_email@gmail.com>
2021-03-15 02:37:11 +0300

    refactor: app design

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Diyetin',
+      title: 'Karatay',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
